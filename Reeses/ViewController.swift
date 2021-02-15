71ae1dbee41a030f56ce316c2b7fcf8b95a5c256
Nipuni Obe <obe@gmail.com>
2021-02-15 18:20:23 -0500

    finished reese's on 2/15 in class

--- Reeses/ViewController.swift
+++ Reeses/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
 
-
+    let message1 = "You got chocolate in my peanut butter!"
+    let message2 = "You got peanut butter in my chocolate!"
+    @IBAction func heyButtonPressed(_ sender: UIButton) {
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = NSTextAlignment.left
+        } else {
+            messageLabel.text = message1
+            messageLabel.textColor = UIColor.systemOrange
+            messageLabel.textAlignment = NSTextAlignment.right
+        }
+    }
+    
 }
 
